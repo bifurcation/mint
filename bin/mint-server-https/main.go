e0f03222bc9ac0df0e520f1f134db9e7b84a1575
Nick Sullivan <nicholas.sullivan@gmail.com>
2016-04-02 23:46:18 -0300

    Support a cert chain in cli.

--- bin/mint-server-https/main.go
+++ bin/mint-server-https/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"crypto"
 	"crypto/x509"
-	"encoding/pem"
-	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -12,6 +10,7 @@ import (
 	"net/http"
 
 	"github.com/bifurcation/mint"
+	"github.com/cloudflare/cfssl/helpers"
 )
 
 var (
@@ -21,39 +20,35 @@ var (
 	keyFile    string
 )
 
-func parsePrivateKey(pemBytes []byte) (crypto.Signer, error) {
-	keyDERBlock, _ := pem.Decode(pemBytes)
-
-	if key, err := x509.ParsePKCS1PrivateKey(keyDERBlock.Bytes); err == nil {
-		return key, nil
-	}
-	if key, err := x509.ParseECPrivateKey(keyDERBlock.Bytes); err == nil {
-		return key, nil
-	}
-	return nil, errors.New("need a DER key")
-}
-
 func main() {
 	flag.StringVar(&port, "port", "4430", "port")
-	flag.StringVar(&serverName, "name", "example.com", "hostname")
-	flag.StringVar(&certFile, "cert", "", "certificate")
-	flag.StringVar(&keyFile, "key", "", "key")
+	flag.StringVar(&serverName, "host", "example.com", "hostname")
+	flag.StringVar(&certFile, "cert", "", "certificate chain in PEM or DER")
+	flag.StringVar(&keyFile, "key", "", "private key in PEM format")
 	flag.Parse()
 
-	var cert []byte
-	var key []byte
+	var certChain []*x509.Certificate
+	var priv crypto.Signer
 	var err error
 
 	if certFile != "" {
-		certPEM, err := ioutil.ReadFile(certFile)
-		if err == nil {
-			var certBlock *pem.Block
-			certBlock, _ = pem.Decode(certPEM)
-			cert = certBlock.Bytes
+		certs, err := ioutil.ReadFile(certFile)
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		} else {
+			certChain, err = helpers.ParseCertificatesPEM(certs)
+			if err != nil {
+				certChain, _, err = helpers.ParseCertificatesDER(certs, "")
+			}
 		}
 	}
 	if keyFile != "" {
-		key, err = ioutil.ReadFile(keyFile)
+		keyPEM, err := ioutil.ReadFile(keyFile)
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		} else {
+			priv, err = helpers.ParsePrivateKeyPEM(keyPEM)
+		}
 	}
 	if err != nil {
 		log.Fatalf("Error: %v", err)
@@ -64,21 +59,11 @@ func main() {
 		ServerName:         serverName,
 	}
 
-	if cert != nil && key != nil {
+	if certChain != nil && priv != nil {
 		log.Printf("Loading cert: %v key: %v", certFile, keyFile)
-		x5cert, err := x509.ParseCertificate(cert)
-		if err != nil {
-			log.Fatalf("Error parsing cert: %v, %v", cert, err)
-		}
-		priv, err := parsePrivateKey(key)
-		if err != nil {
-			log.Fatalf("Error parsing key: %v", key, err)
-		}
-
-		log.Printf("x5cert %v", x5cert.DNSNames)
 		config.Certificates = []*mint.Certificate{
 			&mint.Certificate{
-				Chain:      []*x509.Certificate{x5cert},
+				Chain:      certChain,
 				PrivateKey: priv,
 			},
 		}
@@ -98,5 +83,5 @@ func main() {
 }
 
 func handleClient(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hi there!")
+	fmt.Fprintln(w, "Welcome to the TLS 1.3 zone!")
 }
