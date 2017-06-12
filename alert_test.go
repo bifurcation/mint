65db0b31ab39165b0d31f29ee50ea8203553f239
EKR <ekr@rtfm.com>
2017-06-12 15:28:34 +0100

    Update alert test

--- alert_test.go
+++ alert_test.go
@@ -7,5 +7,5 @@ import (
 func TestAlert(t *testing.T) {
 	assertEquals(t, AlertCloseNotify.String(), "close notify")
 	assertEquals(t, AlertCloseNotify.Error(), "close notify")
-	assertEquals(t, Alert(0xfe).String(), "alert(254)")
+	assertEquals(t, Alert(0xfd).String(), "alert(253)")
 }
