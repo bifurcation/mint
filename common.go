06ca512c2c6bac9cb279fe2c3386343d6b40dce3
Martin Thomson <martin.thomson@gmail.com>
2018-11-05 11:59:05 +0700

    RFC 8446

--- common.go
+++ common.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	supportedVersion  uint16 = 0x7f1c // draft-28
+	supportedVersion  uint16 = 0x0304
 	tls12Version      uint16 = 0x0303
 	tls10Version      uint16 = 0x0301
 	dtls12WireVersion uint16 = 0xfefd
